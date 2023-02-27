84e4ebb0cbd1d8aa704a6ed0a1914bc902135a29
KutuMobaa <kitojawir@gmail.com>
2023-02-27 22:50:29 +0700

    Bebas

---
+++ tutor.py
@@ -0,0 +1,28 @@
+import os,time,sys
+
+
+def menu1():
+    os.system("clear")
+    tentang()
+    tentang1()
+    print(" ")
+    print(" ")
+    entr = input(" tekan enter untuk membuka ")
+    os.system("xdg-open https://youtube.com/@KutuMoba")
+    print(" ")
+    time.sleep(4)
+    print(" terima kasih sudah berkunjung ")
+    print(" ")
+def tentang():
+    print(" ")
+    print(" ")
+    print(" sudah teruji dengan banyak pengguna")
+
+def tentang1():
+    print(" ")
+    print(" ")
+    print(" pastikan google crhome terhubhng ke interntet")
+menu1()
+
+# Author  : Ardia Trista
+# YouTube : Kutu Moba
